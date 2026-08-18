a32074d72f7af03fbba004b79beedcc96ff86e43
Roka0430 <noxx0430@gmail.com>
2026-08-19 08:58:40 +0900

    feat: add no category error handling

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -4,6 +4,11 @@ import Category from "./api/Category.js";
 const initCurrentCategoryId = async () => {
   const categories = await Category.getCategories();
 
+  if (categories.length === 0) {
+    AppState.setCurrentCategoryId(null);
+    return null;
+  }
+
   const categoryId = Math.min(...categories.map(({ id }) => id));
   AppState.setCurrentCategoryId(categoryId);
   return categoryId;
@@ -11,11 +16,14 @@ const initCurrentCategoryId = async () => {
 
 const loadSubjects = async () => {
   const categoryId = AppState.getCurrentCategoryId() ?? (await initCurrentCategoryId());
+  if (categoryId === null) return [];
 
   try {
     return await Category.getSubjects(categoryId);
   } catch {
     const categoryId = await initCurrentCategoryId();
+    if (categoryId === null) return [];
+
     return await Category.getSubjects(categoryId);
   }
 };
@@ -26,6 +34,7 @@ document.addEventListener("alpine:init", () => {
 
     async init() {
       this.subjects = await loadSubjects();
+      console.log();
     },
   }));
 });

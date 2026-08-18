47ae46997f6b149805e40bdaed3cc0a97dc533e0
Roka0430 <noxx0430@gmail.com>
2026-08-19 08:46:19 +0900

    fix: delete routing for edit page

--- index.js
+++ index.js
@@ -12,10 +12,6 @@ app.get("/study/:id", (req, res) => {
   res.sendFile(path.resolve("public/study/index.html"));
 });
 
-app.get("/edit/:id", (req, res) => {
-  res.sendFile(path.resolve("public/edit/index.html"));
-});
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });

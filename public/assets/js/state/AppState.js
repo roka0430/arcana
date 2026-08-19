f243ba949a141a4b3065ed19075aa12a944ab523
Roka0430 <noxx0430@gmail.com>
2026-08-19 20:25:50 +0900

    feat: add localstorage method for study setting

--- public/assets/js/state/AppState.js
+++ public/assets/js/state/AppState.js
@@ -1,5 +1,6 @@
 export default class AppState {
   static #CURRENT_CATEGORY_ID = "arcana:current_category_id";
+  static #STUDY_SETTING = "arcana:study_setting";
 
   static get(key) {
     const value = localStorage.getItem(key);
@@ -17,11 +18,23 @@ export default class AppState {
     localStorage.setItem(key, JSON.stringify(value));
   }
 
+  // Current category ID
+
   static getCurrentCategoryId() {
     return this.get(this.#CURRENT_CATEGORY_ID);
   }
 
   static setCurrentCategoryId(id) {
     this.set(this.#CURRENT_CATEGORY_ID, id);
+  }
+
+  // Study setting
+
+  static getStudySetting() {
+    return this.get(this.#STUDY_SETTING);
+  }
+
+  static setStudySetting(id) {
+    this.set(this.#STUDY_SETTING, id);
   }
 }

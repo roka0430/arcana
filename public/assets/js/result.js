ae582b107f958aab4f5b346ed893bb430048558e
Roka0430 <noxx0430@gmail.com>
2026-08-21 20:06:14 +0900

    feat: initialize result page on load

--- public/assets/js/result.js
+++ public/assets/js/result.js
@@ -1,1 +1,18 @@
-console.log("Welcome to result!");
+import AppState from "./state/AppState.js";
+import Category from "./api/Category.js";
+
+document.addEventListener("alpine:init", () => {
+  Alpine.data("main", () => ({
+    init() {
+      const result = AppState.getStudyResult();
+
+      if (result === null) {
+        location.replace("/");
+        return;
+      }
+
+      AppState.setStudyResult(null);
+      console.log(result);
+    },
+  }));
+});

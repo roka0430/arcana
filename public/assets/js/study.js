185ea280acc127fc50b94fea2bc99986c13492ae
Roka0430 <noxx0430@gmail.com>
2026-08-21 11:34:11 +0900

    fix: make randomShuffle non-mutating

--- public/assets/js/study.js
+++ public/assets/js/study.js
@@ -15,11 +15,14 @@ const loadSubject = async () => {
 };
 
 const randomShuffle = (array) => {
-  for (let i = array.length - 1; i > 0; i--) {
+  const result = [...array];
+
+  for (let i = result.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
+    [result[i], result[j]] = [result[j], result[i]];
   }
-  return array;
+
+  return result;
 };
 
 // Alpine.js

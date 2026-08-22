8de577ad87de0406da565a2de6fffbf1cab1fca8
Roka0430 <noxx0430@gmail.com>
2026-08-22 16:38:35 +0900

    feat: add loading category data on edit page

--- public/assets/js/edit.js
+++ public/assets/js/edit.js
@@ -3,11 +3,31 @@ import Shortcut from "./shortcut/Shortcut.js";
 import ShortcutBar from "./shortcut/ShortcutBar.js";
 import Category from "./api/Category.js";
 
+const initCurrentCategoryId = (categories) => {
+  const categoryId = Math.min(...categories.map(({ id }) => id));
+  AppState.setCurrentCategoryId(categoryId);
+  return categoryId;
+};
+
+const getCurrentCategory = async (categories) => {
+  const categoryId = AppState.getCurrentCategoryId() ?? initCurrentCategoryId(categories);
+
+  try {
+    return await Category.getCategory(categoryId);
+  } catch {
+    const categoryId = initCurrentCategoryId(categories);
+    return await Category.getCategory(categoryId);
+  }
+};
+
 document.addEventListener("alpine:init", () => {
   Alpine.data("shortcutBar", ShortcutBar);
 
   Alpine.data("main", () => ({
-    init() {
+    categories: [],
+    currentCategory: null,
+
+    async init() {
       Shortcut.setContext("edit");
       Shortcut.setOrder("edit", []);
 
@@ -17,6 +37,17 @@ document.addEventListener("alpine:init", () => {
         description: "dummy",
         handler: null,
       });
+
+      this.categories = await Category.getCategories();
+
+      if (this.categories.length === 0) {
+        AppState.setCurrentCategoryId(null);
+        location.replace("/edit/category");
+        return;
+      }
+
+      this.currentCategory = await getCurrentCategory(this.categories);
+      console.log(this.currentCategory);
     },
   }));
 });

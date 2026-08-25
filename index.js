148474b00af46791fffdd067c315b96fc427ff69
Roka0430 <noxx0430@gmail.com>
2026-08-25 16:59:34 +0900

    refactor: change port 3000 -> 3002

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import path from "path";
 import routes from "./api/routes.js";
 
 const app = express();
-const PORT = 3000;
+const PORT = 3002;
 
 app.use(express.json());
 app.use(express.static("public"));
